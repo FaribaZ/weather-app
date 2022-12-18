27a333be9be5c0a825c1b287d88202163d329606
FaribaZ <111224004+FaribaZ@users.noreply.github.com>
2022-12-18 17:04:22 +0330

    show temp

--- src/index.js
+++ src/index.js
@@ -1,21 +1,105 @@
-let now = new Date();
-let days = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
+/// Date
+let today = new Date();
+let options = {
+  weekday: "long",
+  month: "short",
+  day: "numeric",
+};
+let current = new Intl.DateTimeFormat("en-US", options).format(today);
+console.log(current);
+
 let currentDate = document.querySelector(".currentDate");
-let day = days[now.getDay()];
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
+currentDate.innerHTML = current;
+
+///Time
+let option = {
+  hour: "numeric",
+  minute: "numeric",
+  hour12: false,
+};
+let time = new Intl.DateTimeFormat("en-US", option).format(today);
+console.log(current);
+let currentTime = document.querySelector(".currentTime");
+currentTime.innerHTML = time;
+
+/// update the name of the city /// weather/////////////////////////////////////////////
+
+function forcast() {
+  let forcastElement = document.querySelector(".forcast");
+  let day = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri"];
+
+  let forecastHTML = `<div class="row">`;
+
+  day.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+              <ul class="weather">
+                <li>${day}</li>
+                <li>🌤</li>
+                <li>27°C</li>
+              </ul>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forcastElement.innerHTML = forecastHTML;
 }
-let minute = now.getMinutes();
-if (minute < 10) {
-  minute = `0${minute}`;
+
+function showTemp(event) {
+  event.preventDefault();
+
+  let cityElement = document.querySelector(".city-name");
+  cityElement.innerHTML = document.querySelector(".search-input").value;
+  let city = document.querySelector(".search-input").value;
+  console.log(city);
+  let apikey = "29a31f92808151fc2e343be37c695642";
+  let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+  fetch(apiurl)
+    .then((response) => response.json())
+    .then((data) => {
+      tempValue = Math.round(data["main"]["temp"]);
+      let temperatureElement = document.querySelector("#temperature");
+      temperatureElement.innerHTML = tempValue;
+      let descValue = data["weather"][0]["description"];
+      let description = document.querySelector(".description");
+      description.innerHTML = descValue;
+      let iconId = data["weather"][0]["icon"];
+      let icon = document.querySelector("#icon");
+      icon.setAttribute("src", `http://openweathermap.org/img/w/${iconId}.png`);
+      lat = data["coord"]["lat"];
+      lon = data["coord"]["lon"];
+      console.log(lat);
+      console.log(lon);
+    });
 }
-currentDate.innerHTML = `${day}, ${hour}:${minute}`;
+let form = document.querySelector(".search-box");
+form.addEventListener("submit", showTemp);
+/////////////////forcast////////////////
+
+///weather
+
+/// change weather
+function changeToFahrenheit(event) {
+  event.preventDefault();
+  cel.classList.remove("active");
+  far.classList.add("active");
+  let temperature = document.querySelector("#temperature");
+  let farTocel = tempValue * (9 / 5) + 32;
+  temperature.innerHTML = Math.round(farTocel);
+}
+function changeToCelsius(event) {
+  event.preventDefault();
+  far.classList.remove("active");
+  cel.classList.add("active");
+  let temperature = document.querySelector("#temperature");
+  let farTemp = (tempValue - 32) * (5 / 9);
+  temperature.innerHTML = tempValue;
+}
+
+let cel = document.querySelector(".cel");
+cel.addEventListener("click", changeToCelsius);
+let far = document.querySelector(".far");
+far.addEventListener("click", changeToFahrenheit);
+
+forcast();
